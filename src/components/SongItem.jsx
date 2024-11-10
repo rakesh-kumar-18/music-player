066ade7369bc0f2df7f54df06305101c240c4e6f
Rakesh Kumar Sahoo <sahoorakeshkumar42@gmail.com>
2024-11-10 13:35:23 +0530

    add drag and drop functionality

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,13 +1,25 @@
 /* eslint-disable react/prop-types */
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 import { useMusicPlayer } from "./MusicPlayerProvider";
 import { FaMusic } from "react-icons/fa";
 
-function SongItem({ index, song }) {
+function SongItem({ id, index, song }) {
     const { playSong, isPlaying, currentSong } = useMusicPlayer();
     const isCurrent = isPlaying && currentSong === song.src;
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
+
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition,
+    };
 
     return (
         <div
+            ref={setNodeRef}
+            style={style}
+            {...attributes}
+            {...listeners}
             onClick={() => playSong(song.src)}
             className={`grid grid-cols-[30px_1fr_150px_80px_1fr] gap-4 items-center cursor-pointer p-1 rounded-md 
             ${isCurrent ? "bg-[#7a0c0c]" : "bg-transparent"} hover:bg-[#5c0a0a]`}
--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { DndContext, useSensor, useSensors, PointerSensor } from "@dnd-kit/core";
+import { rectSortingStrategy, SortableContext, arrayMove } from "@dnd-kit/sortable";
 import SongItem from "./SongItem";
 import One from "../assets/1.png";
 import Three from "../assets/3.png";
@@ -5,7 +8,7 @@ import Four from "../assets/4.png";
 import Five from "../assets/5.png";
 
 function SongList() {
-    const songs = [
+    const initialSongs = [
         {
             id: 1,
             title: "Billie Jean",
@@ -13,7 +16,7 @@ function SongList() {
             playCount: "1,040,811,084",
             duration: "4:53",
             album: "Thriller 25 Super Deluxe Edition",
-            image: One
+            image: One,
         },
         {
             id: 2,
@@ -22,7 +25,7 @@ function SongList() {
             playCount: "643,786,045",
             duration: "4:18",
             album: "Thriller 25 Super Deluxe Edition",
-            image: One
+            image: One,
         },
         {
             id: 3,
@@ -31,7 +34,7 @@ function SongList() {
             playCount: "407,234,004",
             duration: "4:17",
             album: "Thriller 25 Super Deluxe Edition",
-            image: Three
+            image: Three,
         },
         {
             id: 4,
@@ -40,7 +43,7 @@ function SongList() {
             playCount: "316,391,952",
             duration: "6:05",
             album: "Bad 25th Anniversary Edition",
-            image: Four
+            image: Four,
         },
         {
             id: 5,
@@ -49,31 +52,47 @@ function SongList() {
             playCount: "268,187,218",
             duration: "3:40",
             album: "Off The Wall",
-            image: Five
+            image: Five,
         },
     ];
 
+    const [songs, setSongs] = useState(initialSongs);
+    const sensors = useSensors(useSensor(PointerSensor));
+
+    const handleDragEnd = (event) => {
+        const { active, over } = event;
+        if (active.id !== over.id) {
+            const oldIndex = songs.findIndex((song) => song.id === active.id);
+            const newIndex = songs.findIndex((song) => song.id === over.id);
+            setSongs((songs) => arrayMove(songs, oldIndex, newIndex));
+        }
+    };
+
     return (
-        <div className="text-[#CFC5C5] p-4 mx-16">
-            <div className="flex justify-between items-center mb-4">
-                <h3 className="text-lg font-bold text-[#F6F6F6]">Popular</h3>
-                <span className="text-sm text-[#CFC5C5] cursor-pointer">See All</span>
-            </div>
+        <DndContext sensors={sensors} onDragEnd={handleDragEnd}>
+            <SortableContext items={songs} strategy={rectSortingStrategy}>
+                <div className="text-[#CFC5C5] p-4 mx-16">
+                    <div className="flex justify-between items-center mb-4">
+                        <h3 className="text-lg font-bold text-[#F6F6F6]">Popular</h3>
+                        <span className="text-sm text-[#CFC5C5] cursor-pointer">See All</span>
+                    </div>
 
-            <div className="grid grid-cols-[30px_1fr_150px_80px_1fr] gap-4 mb-2 text-sm font-semibold pb-2">
-                <span>#</span>
-                <span>TITLE</span>
-                <span>PLAYING</span>
-                <span>TIME</span>
-                <span className="text-right">ALBUM</span>
-            </div>
+                    <div className="grid grid-cols-[30px_1fr_150px_80px_1fr] gap-4 mb-2 text-sm font-semibold pb-2">
+                        <span>#</span>
+                        <span>TITLE</span>
+                        <span>PLAYING</span>
+                        <span>TIME</span>
+                        <span className="text-right">ALBUM</span>
+                    </div>
 
-            <div>
-                {songs.map((song, index) => (
-                    <SongItem key={index} index={song.id} song={song} />
-                ))}
-            </div>
-        </div>
+                    <div>
+                        {songs.map((song, index) => (
+                            <SongItem key={song.id} id={song.id} index={index + 1} song={song} />
+                        ))}
+                    </div>
+                </div>
+            </SortableContext>
+        </DndContext>
     );
 }
 
